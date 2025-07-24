6d0d4b4abd423268e3f695cdc989538c1d2b1749
RalphsGit <ralph.nassar@mail.mcgill.ca>
2025-07-24 15:17:49 +0300

    Added Compare Method

--- assignment_July_23/Employee.cpp
+++ assignment_July_23/Employee.cpp
@@ -1,39 +1,54 @@
 #include<iostream>
 using namespace std;
 
-class Employee{
-    
+class Employee {
     static int counter;
     int empid;
     char *name;
     double salary;
 
-    public:
-    void getdata(char *ename, double sal){
+public:
+    void getdata(char *ename, double sal) {
         empid = ++counter;
         name = ename;
         salary = sal;
     }
-    void printEmployee(void);
-   
-    static void generateEmpid(){
+
+    void printEmployee(void) {
+        cout << "\nEmpid : " << empid << endl;
+        cout << "Name : " << name << endl;
+        cout << "salary: " << salary << endl;
+    }
+
+    Employee compareSalary(Employee other) {
+        if (this->salary > other.salary) {
+            cout << name << " has a higher salary than " << other.name << "." << endl;
+            return *this;
+        }
+            
+        else if (this->salary < other.salary) {
+            cout << other.name << " has a higher salary than " << name << "." << endl;
+            return other;
+        }
+            
+        else {
+            cout << name << " and " << other.name << " have the same salary." << endl;
+            return *this;
+        }
+            
+    }
+
+    static void generateEmpid() {
         counter = 2000;
     }
-
 };
-int Employee:: counter;
-void Employee :: printEmployee(void) // member function
-    {
-        cout<<"\nEmpid : "<<empid<<endl;
-        cout<<"Name : "<<name<<endl;
-        cout<<"salary: "<<salary<<endl;
-    }
 
-int main(){
-    Employee e1,e2,e3;
+int Employee::counter;
 
-    Employee:: generateEmpid();
-    
+int main() {
+    Employee e1, e2, e3;
+    Employee::generateEmpid();
+
     char* name1 = new char[100];
     char* name2 = new char[100];
     char* name3 = new char[100];
@@ -60,14 +75,15 @@ int main(){
     e1.printEmployee();
     e2.printEmployee();
     e3.printEmployee();
-    
+
+    cout << "\nSalary comparison:\n";
+    e1.compareSalary(e2);
+    e2.compareSalary(e3);
+    e1.compareSalary(e3);
+
     delete[] name1;
     delete[] name2;
     delete[] name3;
 
     return 0;
 }
-
-// Compare two objects
-// Define a functions, take two parameters, two objects (aka who has higher salary.)
-
